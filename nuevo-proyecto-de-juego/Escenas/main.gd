50dfef2325c1fec1e851f584a9e038a7500b8ef4
PipitaFCO <juampymartinez06@gmail.com>
2026-06-19 17:29:20 -0300

    ahora si

--- nuevo-proyecto-de-juego/Escenas/main.gd
+++ nuevo-proyecto-de-juego/Escenas/main.gd
@@ -2,32 +2,50 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
+	get_tree().paused = true
+	pass # Replace with function body.
+
+
+
+func _on_menu_jugar():
+	get_tree().paused= false
+	$Menu.visible = false
+	pass # Replace with function body.
+
+
+func _on_menu_rejugar():
+	get_tree().paused = true
+	$Menu.visible = false
+	pass # Replace with function body.
+
+func _on_menu_salir():
 	get_tree().paused = false
+	$Menu.visible = false
 	pass # Replace with function body.
 
-func _on_bandera_enemigo_pierde() -> void:
-	$Personaje.queue_free()
-	pass # Replace with function body.
 
-func _on_bandera_personaje_gana() -> void:
+
+
+func _on_area_2d_body_entered(body: Node2D):
+	if body.name == "Personaje":
+		$Enemigo.queue_free()
+		$Bandera.queue_free()
+	pass # Replace with function body.
+	
+	if body.name == "Enemigo":
+		$Personaje.queue_free()
+		pass
+
+
+
+func _on_bandera_gana():
 	$Enemigo.queue_free()
 	$Bandera.queue_free()
 	get_tree().paused = true
 	pass # Replace with function body.
 
-func _on_menu_jugar() -> void:
-	get_tree().paused = false
-	$Menu.visible = false
-	pass # Replace with function body.
 
-
-func _on_menu_rejugar() -> void:
-	get_tree().paused = false
-	$Menu.visible = false
-	pass # Replace with function body.
-
-func _on_menu_salir() -> void:
-	get_tree().paused = false
-	$Menu.visible = false
+func _on_bandera_pierde():
+	$Personaje.queue_free()
 	pass # Replace with function body.
--- nuevo-proyecto-de-juego/Escenas/menu.gd
+++ nuevo-proyecto-de-juego/Escenas/menu.gd
@@ -1,19 +1,18 @@
 extends Node2D
 
-signal Jugar
-signal Rejugar
-signal Salir
+signal jugar
+signal rejugar
+signal salir
 
 func _on_jugar_pressed() -> void:
 	emit_signal("jugar")
 	pass # Replace with function body.
 
-
-func _on_rejugar_pressed() -> void:
+func _on_rejugar_pressed():
 	emit_signal("rejugar")
 	pass # Replace with function body.
 
 
-func _on_salir_pressed() -> void:
+func _on_salir_pressed():
 	emit_signal("salir")
 	pass # Replace with function body.
